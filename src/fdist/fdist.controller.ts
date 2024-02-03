3cca7697351bb25dcdbfe60143576e4642e59e0c
ymHan <onyxsard@gmail.com>
2024-02-03 14:28:01 +0900

    [ymhan]

--- src/fdist/fdist.controller.ts
+++ src/fdist/fdist.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Inject, OnModuleInit, Get } from '@nestjs/common';
+import { Controller, Inject, OnModuleInit, Get, Param } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
-import { F_DIST_SERVICE_NAME, FDistServiceClient, GetVideoListResponse } from '@proto/fdist.pb';
+import {
+  F_DIST_SERVICE_NAME,
+  FDistServiceClient,
+  GetVideoByIdRequest,
+  GetVideoByIdResponse,
+  GetVideoListResponse,
+} from '@proto/fdist.pb';
 
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiParam,
+  ApiOperation,
+  ApiConsumes,
+  ApiBody,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiQuery,
+} from '@nestjs/swagger';
+import { LeaveMemberRequest } from '@proto/member.pb';
 @ApiTags('FDist - Video')
 @Controller({ path: 'video' })
 export class FDistController implements OnModuleInit {
@@ -15,9 +31,21 @@ export class FDistController implements OnModuleInit {
   onModuleInit() {
     this.svc = this.client.getService<FDistServiceClient>(F_DIST_SERVICE_NAME);
   }
+  
+  @ApiOperation({ summary: '영상 전체 목록' })
   @Get('videos')
   public getVideos(): Observable<GetVideoListResponse> {
-    console.log('getVideos');
     return this.svc.getVideos({});
+  }
+  @ApiOperation({ summary: '영상 정보 조회' })
+  @ApiParam({
+    name: 'id',
+    description: '영상 ID',
+    required: true,
+    type: 'number',
+  })
+  @Get('videos/:id')
+  public getVideoById(@Param() params: GetVideoByIdRequest): Observable<any> {
+    return this.svc.getVideoById(params);
   }
 }
