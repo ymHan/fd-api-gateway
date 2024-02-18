88b221ac1e696de4f0cfce4465520b4edadbbaff
ymHan <onyxsard@gmail.com>
2024-02-18 15:30:54 +0900

    [ymhan]

--- src/backoffice/backoffice.video.controller.ts
+++ src/backoffice/backoffice.video.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, OnModuleInit, Post, Get, Req, Param } from '@nestjs/common';
+import { Body, Controller, Inject, OnModuleInit, Post, Get, Req, Param, Query } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import {
@@ -7,10 +7,10 @@ import {
   V1CreateVideoRequest,
   V1CreateVideoResponse,
   V1GetVideoRequest,
-  V1GetVideoResponse,
+  V1GetVideoResponse, V1ListVideoResponse,
 } from '@proto/backoffice.pb';
 import { Category, CategorySubEnum, CategorySubCodeEnum, RecordType } from '@root/models/enum';
-import { ApiConsumes, ApiTags, ApiBody, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { ApiConsumes, ApiTags, ApiBody, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @ApiTags('BackOffice - Video')
 @Controller({ path: 'bo' })
@@ -96,5 +96,44 @@ export class VideoController implements OnModuleInit {
   })
   public getVideo(@Param() params: V1GetVideoRequest): Observable<V1GetVideoResponse> {
     return this.svc.v1GetVideo(params);
+  }
+
+  @Get('videos')
+  @ApiOperation({ summary: 'Get videos', description: 'Get videos' })
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: false,
+    description: 'page number',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'number',
+    required: false,
+    description: 'limit',
+  })
+  @ApiQuery({
+    name: 'sort',
+    type: 'string',
+    required: false,
+    description: 'sort',
+  })
+  @ApiQuery({
+    name: 'order',
+    type: 'string',
+    required: false,
+    description: 'order',
+  })
+  public listVideos(
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+    @Query('sort') sort: string,
+    @Query('order') order: string): Observable<any> {
+    const payload = { page, limit, sort, order };
+    return this.svc.v1ListVideo(payload);
+  }
+
+  public delVideos(@Req() req: Request, @Body() payload: V1CreateVideoRequest): Observable<V1CreateVideoResponse> {
+    return this.svc.v1CreateVideo(payload);
   }
 }
